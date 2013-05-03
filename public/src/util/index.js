806f857a1c7e71e15ecea3046fb907072d0769a9
HC Chen <1070292024@qq.com>
2013-05-03 11:41:08 +0800

    util--index.js文件失效

--- public/src/util/index.js
+++ public/src/util/index.js
@@ -5,13 +5,14 @@
  * 表单工具方法调用
  */
 define(function (require, exports, modules) {
+/*	
     var $ = require('jquery')
-		, $sunit = $('#sunit');
+        , $sunit = $('#sunit');
 	
-    $('#setdate').datetimepicker({	    //时间控件调用
+    $('#setdate').datetimepicker({             //时间控件调用
         pickTime: false
     });
-/*
+
     $sunit.change(function(e) {	//针对所属单位进行判断
         var sel = $sunit.val();
         sel === '0' ? $('#unit').show() : $('#unit').hide();
