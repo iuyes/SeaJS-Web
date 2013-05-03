c1a4ad3e5c858f6133a8c23f98f2d58aa450e356
HC Chen <1070292024@qq.com>
2013-05-03 11:38:12 +0800

    添加注释SeaJS config配置、引用说明

--- public/src/conf/index-config.js
+++ public/src/conf/index-config.js
@@ -5,6 +5,10 @@
  * seajs配置文件
  */
 
+//采用data-main="index-config.js"，而非data-main="index-main.js" 
+//首先载入配置文件(本文件) ，在去获取主入口文件
+//解决SeaJS V1.3.1 seajs.config 需在下次seajs.use才载入运行
+
 seajs.config({
     alias: {
         'jquery': 'jquery/1.8.2/jquery-debug',
@@ -14,7 +18,7 @@ seajs.config({
         'backbone' : 'backbone/1.0.0/backbone-debug',
         'handlebars' : 'handlebars/1.1.1/handlebars-debug',
         'json' : 'json2/1.0.2/json2-debug',
-		'observer': 'observer/1.0.0/observer'
+        'observer': 'observer/1.0.0/observer'
     }
     , preload:['jquery', 'plugin-text']
 });
