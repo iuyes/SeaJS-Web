6e2b803d3098e0e9fd2c1f284792d30d4a57f38d
HC Chen <chceyes@gmail.com>
2013-11-11 20:14:01 +0800

    observer添加past函数 和 debug信息输出

--- public/sea-modules/observer/1.0.0/observer.js
+++ public/sea-modules/observer/1.0.0/observer.js
@@ -1,10 +1,9 @@
 define(function(require, exports, modules) {
 
     var object = exports;
-    var eventSplitter = /\s+/;
     var stack = {};
-    var console = window.console || function() {};
     var _events = {};
+    var console = window.console || function() {};
 
     modules.exports = {
         on: function(eventName, callback, context) {
@@ -14,7 +13,8 @@ define(function(require, exports, modules) {
             _event.push({
                 callback: callback,
                 context: context,
-                ctx: context || this
+                ctx: context || this,
+                stackPath: debugTriggerObserver()
             });
             return this;
         },
@@ -46,23 +46,47 @@ define(function(require, exports, modules) {
             return this;
         },
 
-        past: function() {},
+        // 先执行事件上次触发
+        past: function(eventName, callback, context) {
+
+            var args = stack[eventName];
+            if (callback) {
+                callback.apply(context, args);
+            }
+
+            return this.on(eventName, callback, context);
+        },
 
         trigger: function(eventName) {
 
             var _event = _events[eventName] || [];
             var args = Array.prototype.slice.call(arguments, 1);
+            stack[eventName] = args;
 
+            console.log('%s 事件触发于: %s', eventName, debugTriggerObserver());
             for (var i = _event.length - 1; i >= 0; i--) {
 
                 var obj = _event[i];
+                var triggerPath = obj.stackPath;
+                console.log('%s 事件订阅于: %s', eventName, triggerPath);
+
                 obj.callback.apply(obj.context || obj.ctx, args);
             };
+
+            return this;
         },
 
         clear: function () {
 
         }
     };
+
+    // 获取函数的堆栈信息
+    function debugTriggerObserver() {
+
+        var e = new Error();
+        var errorStack = e.stack;
+        return errorStack.split('\n')[3];
+    }
 
 });
