83ebedc6c24e010fd1824271d830910471919cfa
Nightink <chceyes@gmail.com>
2013-12-09 15:08:06 +0800

    更新websql 类库

--- public/src/webdb/db.js
+++ public/src/webdb/db.js
@@ -6,41 +6,62 @@
  */
 define(function (require, exports, module) {
 
-    var _ = require('underscore');
     var alert = window.alert;
-    var console = window.console;
+
+    var console = window.console || {
+
+        log: function() {}
+    };
+
+    var toString = Object.prototype.toString;
+    // 工具方法
+    var _ = {
+        isFunction: function(obj) {
+
+            return toString.call(obj) === '[object Function]';
+        },
+        isString: function(obj) {
+
+            return toString.call(obj) === '[object String]';
+        },
+        isObject: function(obj) {
+
+            return toString.call(obj) === '[object Object]';
+        }
+    };
+
+    var openDatabase = window.openDatabase;
+
+    if (!openDatabase) {
+
+        alert('web database doesn\'t support');
+        return;
+    }
 
     //WebDb构造函数
     function WebDb(dbName, version, displayName, maxSize) {
-        var len = arguments.length;
+
         try {
-            if (!window.openDatabase) {
-                alert('本地数据库不支持');
-            } else {
-                if (len < 4) {
-                    alert('参数至少要四个');
-                    return;
-                }
-                //数据库单例模式
-                if (!this.db) {
-                    this.db = openDatabase(dbName, version, displayName, maxSize);
-                } else {
-                    return this;
-                }
-            }
+
+            this.db = openDatabase(dbName, version, displayName, maxSize);
         } catch (e) {
+
             if (e === 2) {
-                console.log('无效数据库版本');
+
+                console.log('Invalid database version');
             } else {
-                console.log('未知错误 ' + e + '。');
+
+                console.log('Unknown error %s。', e);
             }
         }
     }
 
     WebDb.prototype = {
-        constructor:WebDb,
+        
+        constructor: WebDb,
+        
         //Sql操作函数
-        exec:function (sql, params, successFn, errorFn) {
+        exec: function(sql, params, successFn, errorFn) {
             if (_.isFunction(params)) {
                 successFn = params;
                 params = undefined;
@@ -52,15 +73,16 @@ define(function (require, exports, module) {
 
             errorFn = errorFn || function (transaction, error) {
                 //设置sql操作失败处理函数
-                console.log('Error was ' + error.message + ' errorCode is ' + error.code);
+                console.log('Error was %s errorCode is %s', error.message, error.code);
             };
 
             this.db.transaction(function (tx) {
                 tx.executeSql(sql, params, successFn, errorFn);
             });
         },
+
         //创建数据库表
-        createTable:function (tableName, tableStruct) {
+        createTable: function(tableName, tableStruct) {
             var structSql = '', name;
             for (name in tableStruct) {
                 structSql += name + ' ' + tableStruct[name] + ' ,';
@@ -68,18 +90,21 @@ define(function (require, exports, module) {
             structSql = structSql.slice(0, -1);
             this.exec('CREATE TABLE IF NOT EXISTS ' + tableName + '(' + structSql + ')');
         },
-        dropTable:function (tableName) {
+
+        dropTable: function(tableName) {
             this.exec('DROP TABLE ' + tableName);
         },
+
         //数据库查找操作
-        select:function (table, successFn, index, page) {
+        select:function(table, successFn, index, page) {
             index = index || 0;
             page = page || 20;
             var self = this, sql = 'SELECT * FROM ' + table + ' LIMIT ' + index + ',' + page;
             self.exec(sql, [], successFn);
         },
+
         // 数据库插入操作
-        insert:function (table, tableObj, successFn) {
+        insert: function(table, tableObj, successFn) {
             var self = this, sql = '',
                 name, nameArr = [], valueArr = [];
             for (name in tableObj) {
@@ -95,8 +120,9 @@ define(function (require, exports, module) {
             sql = 'INSERT INTO ' + table + '(' + nameArr.join(',') + ') VALUES(' + valueArr.join(',') + ');';
             self.exec(sql, successFn);
         },
+
         //数据库更新操作
-        update:function (table, tableObj, falg, successFn) {
+        update: function(table, tableObj, falg, successFn) {
             var self = this,
                 sql = 'UPDATE ' + table + ' SET ';
 
@@ -120,17 +146,20 @@ define(function (require, exports, module) {
             }
             self.exec(sql, successFn);
         },
-        delete:function (table, id, successFn) {
+
+        delete: function(table, id, successFn) {
             var sql = 'DELETE FROM ' + table + ' WHERE id = ' + id + ';';
             this.exec(sql, successFn);
         },
+
         // 获得表数据个数
-        count:function (table, countFn) {
+        count: function(table, countFn) {
             var sql = 'SELECT count(*) FROM ' + table;
             this.exec(sql, countFn);
         },
+
         // like操作
-        like:function (table, tableObj, successFn, index, page) {
+        like: function(table, tableObj, successFn, index, page) {
             index = index || 0;
             page = page || 5;
 
